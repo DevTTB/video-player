7791b74d80866cc432f2f1d3f07d2cf66472054b
devttb <designerttb@gmail.com>
2025-07-08 11:29:09 +0700

    Feature mute

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -1,88 +1,161 @@
-import { AspectRatio, Box, Flex, IconButton, Slider, Span } from "@chakra-ui/react";
-import { FaPlay, FaPause } from "react-icons/fa6";
-import { TbPlayerSkipBack, TbPlayerSkipForward } from "react-icons/tb";
-import { SlVolume1, SlVolume2, SlVolumeOff } from "react-icons/sl";
-import { GoScreenFull } from "react-icons/go";
-import { RiFullscreenExitLine } from "react-icons/ri";
-import { IoSettingsOutline } from "react-icons/io5";
+import {Box, Flex, IconButton, Slider, Span} from "@chakra-ui/react";
+import {FaPlay, FaPause} from "react-icons/fa6";
+import {TbPlayerSkipBack, TbPlayerSkipForward} from "react-icons/tb";
+import {SlVolume1, SlVolume2, SlVolumeOff} from "react-icons/sl";
+import {GoScreenFull} from "react-icons/go";
+import {RiFullscreenExitLine} from "react-icons/ri";
+import {IoSettingsOutline} from "react-icons/io5";
 
-import { useRef } from "react";
-import { useVideoPlayer } from "@/hooks/use-video-player/use-video-player";
-import { useHotkeys } from "@/hooks/use-hotkeys/use-hotkeys";
+import {useRef} from "react";
+import {useVideoPlayer} from "@/hooks/use-video-player/use-video-player";
+import {useHotkeys} from "@/hooks/use-hotkeys/use-hotkeys";
 
 interface IVideoPlayerProps {
-    src: string;
+  src: string;
 }
 
+export const VideoPlayer: React.FC<IVideoPlayerProps> = ({src}) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-export const VideoPlayer: React.FC<IVideoPlayerProps> = ({ src }) => {
-    const videoRef = useRef<HTMLVideoElement>(null);
-    const containerRef = useRef<HTMLDivElement>(null);
+  const {videoState, handlePlay, handleFullScreen, handleSeek, handleChangeVolume, hanldeMute, formatTime} =
+    useVideoPlayer(videoRef);
 
-    const { videoState, handlePlay, handleFullScreen, handleSeek, handleChangeVolume, formatTime } = useVideoPlayer(videoRef);
-    useHotkeys([['space', handlePlay], ['f', () => handleFullScreen(containerRef)]]);
+  useHotkeys([
+    ["space", handlePlay],
+    ["f", () => handleFullScreen(containerRef)],
+    ["arrowLeft", () => handleSeek(videoState.currentTime - 5)],
+    ["arrowRight", () => handleSeek(videoState.currentTime + 5)],
+    ["arrowUp", () => handleChangeVolume(Math.min(videoState.volume + 0.1, 1))],
+    ["arrowDown", () => handleChangeVolume(Math.max(videoState.volume - 0.1, 0))],
+    ["m", hanldeMute],
+  ]);
 
-    return (
-        <Box position={"relative"} width={"100%"} height={"100%"} ref={containerRef}>
+  return (
+    <Box position={"relative"} width={"100%"} height={"100%"} ref={containerRef}>
+      <Flex
+        width={"100%"}
+        height={"100%"}
+        justify={"center"}
+        onClick={handlePlay}
+        onDoubleClick={() => handleFullScreen(containerRef)}
+      >
+        <video ref={videoRef} src={src} />
+      </Flex>
 
-            <Flex width={"100%"} height={"100%"} justify={"center"} onClick={handlePlay} onDoubleClick={() => handleFullScreen(containerRef)} >
-                <video ref={videoRef} src={src} />
+      <Box position={"absolute"} bottom={"0px"} width={"100%"}>
+        <Slider.Root
+          min={0}
+          max={videoState.duration}
+          onValueChange={(e) => {
+            handleSeek(e.value[0]);
+          }}
+          value={[videoState.currentTime]}
+          size={"sm"}
+          paddingX={"10px"}
+        >
+          <Slider.Control>
+            <Slider.Track bg="rgba(255, 255, 255, .2)">
+              <Slider.Range bg="red.600" />
+            </Slider.Track>
+            <Slider.Thumb
+              index={0}
+              borderColor="red.600"
+              bg={"red.600"}
+              _focusVisible={{boxShadow: "none"}}
+            ></Slider.Thumb>
+          </Slider.Control>
+        </Slider.Root>
+
+        <Flex justify-content={"space-between"} textShadow={"0 0 2px rgba(0,0,0,.5)"}>
+          <Flex flex={"1"}>
+            <IconButton
+              _hover={{background: "gray"}}
+              variant={"ghost"}
+              color={"white"}
+              size={"2xl"}
+              onClick={handlePlay}
+            >
+              <FaPause strokeWidth={"1.5"} />
+            </IconButton>
+            <IconButton
+              onClick={() => handleSeek(videoState.currentTime - 5)}
+              _hover={{background: "gray"}}
+              variant={"ghost"}
+              color={"white"}
+              size={"2xl"}
+            >
+              <TbPlayerSkipBack strokeWidth={"1.5"} />
+            </IconButton>
+            <IconButton
+              onClick={() => handleSeek(videoState.currentTime + 5)}
+              _hover={{background: "gray"}}
+              variant={"ghost"}
+              color={"white"}
+              size={"2xl"}
+            >
+              <TbPlayerSkipForward strokeWidth={"1.5"} />
+            </IconButton>
+            <IconButton
+              _hover={{background: "gray"}}
+              variant={"ghost"}
+              color={"white"}
+              size={"2xl"}
+              onClick={hanldeMute}
+            >
+              {videoState.isMuted || videoState.volume == 0 ? (
+                <SlVolumeOff />
+              ) : videoState.volume >= 0.5 ? (
+                <SlVolume2 strokeWidth={"1.5"} />
+              ) : (
+                <SlVolume1 strokeWidth={"1.5"} />
+              )}
+            </IconButton>
+            <Flex width={"70px"} alignItems={"center"}>
+              <Slider.Root
+                value={[videoState.volume]}
+                onValueChange={(e) => handleChangeVolume(e.value[0])}
+                max={1}
+                step={0.1}
+                size={"sm"}
+                width={"100%"}
+              >
+                <Slider.Control>
+                  <Slider.Track bg="rgba(255, 255, 255, .2)" height={"4px"}>
+                    <Slider.Range bg="white" />
+                  </Slider.Track>
+                  <Slider.Thumb
+                    index={0}
+                    borderColor="white"
+                    bg={"white"}
+                    _focusVisible={{boxShadow: "none"}}
+                    boxSize={3}
+                  ></Slider.Thumb>
+                </Slider.Control>
+              </Slider.Root>
             </Flex>
-
-            <Box position={"absolute"} bottom={"0px"} width={"100%"}>
-
-                <Slider.Root min={0} max={videoState.duration} onValueChange={(e) => { handleSeek(e.value[0]); }} value={[videoState.currentTime]} size={"sm"} paddingX={"10px"} >
-                    <Slider.Control>
-                        <Slider.Track bg="rgba(255, 255, 255, .2)">
-                            <Slider.Range bg="red.600" />
-                        </Slider.Track>
-                        <Slider.Thumb index={0} borderColor="red.600" bg={"red.600"} _focusVisible={{ boxShadow: "none" }} >
-                        </Slider.Thumb>
-                    </Slider.Control>
-                </Slider.Root>
-
-                <Flex justify-content={"space-between"} textShadow={"0 0 2px rgba(0,0,0,.5)"}>
-                    <Flex flex={'1'}>
-                        <IconButton _hover={{ background: "gray" }} variant={"ghost"} color={"white"} size={"2xl"} onClick={handlePlay} >
-                            <FaPause strokeWidth={"1.5"} />
-                        </IconButton>
-                        <IconButton _hover={{ background: "gray" }} variant={"ghost"} color={"white"} size={"2xl"} >
-                            <TbPlayerSkipBack strokeWidth={"1.5"} />
-                        </IconButton>
-                        <IconButton _hover={{ background: "gray" }} variant={"ghost"} color={"white"} size={"2xl"} >
-                            <TbPlayerSkipForward strokeWidth={"1.5"} />
-                        </IconButton>
-                        <IconButton _hover={{ background: "gray" }} variant={"ghost"} color={"white"} size={"2xl"} >
-                            <SlVolume1 strokeWidth={"1.5"} />
-                        </IconButton>
-                        <Flex width={"70px"} alignItems={"center"}>
-                            <Slider.Root value={[videoState.volume]} onValueChange={(e) => handleChangeVolume(e.value[0])} max={1} step={0.1} size={"sm"} width={"100%"}>
-                                <Slider.Control>
-                                    <Slider.Track bg="rgba(255, 255, 255, .2)" height={"4px"}>
-                                        <Slider.Range bg="white" />
-                                    </Slider.Track>
-                                    <Slider.Thumb index={0} borderColor="white" bg={"white"} _focusVisible={{ boxShadow: "none" }} boxSize={3}>
-                                    </Slider.Thumb>
-                                </Slider.Control>
-                            </Slider.Root>
-                        </Flex>
-                        <Box color={"white"} fontSize={"14px"} display={"flex"} alignItems={"center"}>
-                            <Span>{formatTime(videoState.currentTime)}</Span>
-                            <Span mx={"6px"}>/</Span>
-                            <Span>{formatTime(videoState.duration)}</Span>
-                        </Box>
-                    </Flex>
-                    <Flex>
-                        <IconButton _hover={{ background: "gray" }} variant={"ghost"} color={"white"} size={"2xl"} >
-                            <IoSettingsOutline strokeWidth={"1.5"} />
-                        </IconButton>
-                        <IconButton _hover={{ background: "gray" }} variant={"ghost"} color={"white"} size={"2xl"} onClick={() => handleFullScreen(containerRef)}>
-                            <GoScreenFull strokeWidth={"1.5"} />
-                        </IconButton>
-                    </Flex>
-
-                </Flex>
+            <Box color={"white"} fontSize={"14px"} display={"flex"} alignItems={"center"}>
+              <Span>{formatTime(videoState.currentTime)}</Span>
+              <Span mx={"6px"}>/</Span>
+              <Span>{formatTime(videoState.duration)}</Span>
             </Box>
-        </Box>
-    )
-}
+          </Flex>
+          <Flex>
+            <IconButton _hover={{background: "gray"}} variant={"ghost"} color={"white"} size={"2xl"}>
+              <IoSettingsOutline strokeWidth={"1.5"} />
+            </IconButton>
+            <IconButton
+              _hover={{background: "gray"}}
+              variant={"ghost"}
+              color={"white"}
+              size={"2xl"}
+              onClick={() => handleFullScreen(containerRef)}
+            >
+              <GoScreenFull strokeWidth={"1.5"} />
+            </IconButton>
+          </Flex>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
--- src/hooks/use-video-player/use-video-player.ts
+++ src/hooks/use-video-player/use-video-player.ts
@@ -1,116 +1,130 @@
 import type React from "react";
-import { useCallback, useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 
 interface IVideoState {
-    isPlaying: boolean;
-    isFullScreen: boolean;
-    currentTime: number;
-    duration: number;
-    volume: number; // from 0 to 1
+  isPlaying: boolean;
+  isFullScreen: boolean;
+  currentTime: number;
+  duration: number; // all time in seconds
+  volume: number; // from 0 to 1
+  isMuted?: boolean; // optional, if you want to track mute state
 }
 
 const initVideoState = {
-    isPlaying: false,
-    isFullScreen: false,
-    currentTime: 0,
-    duration: 0,
-    volume: 0.5,
-}
+  isPlaying: false,
+  isFullScreen: false,
+  currentTime: 0,
+  duration: 0,
+  volume: 1,
+  isMuted: false,
+};
 
 export function useVideoPlayer(videoRef: React.RefObject<HTMLVideoElement | null>) {
-    const [videoState, setVideoState] = useState<IVideoState>(initVideoState);
+  const [videoState, setVideoState] = useState<IVideoState>(initVideoState);
 
-    const updateVideoSate = useCallback((newState: Partial<IVideoState>) => {
-        setVideoState(prevState => ({ ...prevState, ...newState }));
-    }, [])
+  const updateVideoSate = useCallback((newState: Partial<IVideoState>) => {
+    setVideoState((prevState) => ({...prevState, ...newState}));
+  }, []);
 
-    const handlePlay = useCallback(() => {
-        const video = videoRef.current;
+  const handlePlay = useCallback(() => {
+    const video = videoRef.current;
 
-        if (!video) return;
+    if (!video) return;
 
-        if (videoState.isPlaying) {
-            video.pause();
-            updateVideoSate({ isPlaying: false });
-            return
-        }
-
-        video.play();
-        updateVideoSate({ isPlaying: true });
-
-    }, [videoState.isPlaying, updateVideoSate, videoRef]);
-
-
-    const handleFullScreen = useCallback((containerRef: React.RefObject<HTMLDivElement | null>) => {
-        const container = containerRef.current;
-
-        if (!container) return;
-
-        if (videoState.isFullScreen) {
-            document.exitFullscreen();
-            updateVideoSate({ isFullScreen: false });
-            return;
-        }
-
-        container.requestFullscreen();
-        updateVideoSate({ isFullScreen: true });
-
-    }, [videoState.isFullScreen, updateVideoSate, videoRef]);
-
-    const formatTime = (seconds: number): string => {
-        const hours = Math.floor(seconds / 3600);
-        const minutes = Math.floor(seconds / 60);
-        const secs = Math.floor(seconds % 60);
-        if (hours > 0) {
-            return `${hours}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-        }
-        return `${minutes}:${secs.toString().padStart(2, '0')}`;
+    if (videoState.isPlaying) {
+      video.pause();
+      updateVideoSate({isPlaying: false});
+      return;
     }
 
-    const handleSeek = useCallback((value: number) => {
-        const video = videoRef.current;
-        if (!video) return;
+    video.play();
+    updateVideoSate({isPlaying: true});
+  }, [videoState.isPlaying, updateVideoSate, videoRef]);
 
-    
-        video.currentTime = value;
-        updateVideoSate({ currentTime: value });
+  const handleFullScreen = useCallback(
+    (containerRef: React.RefObject<HTMLDivElement | null>) => {
+      const container = containerRef.current;
 
-    }, [videoState.currentTime, updateVideoSate, videoRef]);
+      if (!container) return;
 
-    const handleChangeVolume = useCallback((value: number) => {
-        const video = videoRef.current;
-        if (!video) return; 
-        
-        video.volume = value;
-        updateVideoSate({ volume: value });
+      if (videoState.isFullScreen) {
+        document.exitFullscreen();
+        updateVideoSate({isFullScreen: false});
+        return;
+      }
 
-    }, [updateVideoSate, videoRef]);
+      container.requestFullscreen();
+      updateVideoSate({isFullScreen: true});
+    },
+    [videoState.isFullScreen, updateVideoSate, videoRef],
+  );
 
-    useEffect(() => {
-        const video = videoRef.current;
-
-        const handleTimeUpdate = () => {
-            updateVideoSate({ currentTime: video?.currentTime })
-        }
-        const handleLoadedMetadata = () => {
-            updateVideoSate({ duration: video?.duration })
-        }
-
-        video?.addEventListener('timeupdate', handleTimeUpdate);
-        video?.addEventListener('loadedmetadata', handleLoadedMetadata);
-
-        return () => {
-            video?.removeEventListener('timeupdate', handleTimeUpdate);
-            video?.removeEventListener('loadedmetadata', handleLoadedMetadata);
-        }
-    }, []);
-
-    return {
-        videoState,
-        handlePlay,
-        handleFullScreen,
-        handleSeek,
-        handleChangeVolume,
-        formatTime,
+  const formatTime = (seconds: number): string => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    if (hours > 0) {
+      return `${hours}:${minutes.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
     }
+    return `${minutes}:${secs.toString().padStart(2, "0")}`;
+  };
+
+  const handleSeek = useCallback(
+    (value: number) => {
+      const video = videoRef.current;
+      if (!video) return;
+
+      video.currentTime = value;
+      updateVideoSate({currentTime: value});
+    },
+    [videoState.currentTime, updateVideoSate, videoRef],
+  );
+
+  const handleChangeVolume = useCallback(
+    (value: number) => {
+      const video = videoRef.current;
+      if (!video) return;
+
+      video.volume = value;
+      updateVideoSate({volume: value, isMuted: value === 0});
+    },
+    [videoState.isMuted, updateVideoSate, videoRef],
+  );
+
+  const hanldeMute = useCallback(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    video.muted = !video.muted;
+    updateVideoSate({isMuted: !videoState.isMuted});
+  }, [videoState.isMuted, updateVideoSate, videoRef]);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const handleTimeUpdate = () => {
+      updateVideoSate({currentTime: video?.currentTime});
+    };
+    const handleLoadedMetadata = () => {
+      updateVideoSate({duration: video?.duration});
+    };
+
+    video?.addEventListener("timeupdate", handleTimeUpdate);
+    video?.addEventListener("loadedmetadata", handleLoadedMetadata);
+
+    return () => {
+      video?.removeEventListener("timeupdate", handleTimeUpdate);
+      video?.removeEventListener("loadedmetadata", handleLoadedMetadata);
+    };
+  }, []);
+
+  return {
+    videoState,
+    handlePlay,
+    handleFullScreen,
+    handleSeek,
+    handleChangeVolume,
+    hanldeMute,
+    formatTime,
+  };
 }
